ad72d62c1734ac481b1e457c06b3a463b7657d80
Noot <101384541+Pingu7731@users.noreply.github.com>
2025-04-17 18:21:28 +0800

    Kamiya's fault
    ?????

--- pomodoro/src/App.vue
+++ pomodoro/src/App.vue
@@ -1,15 +1,22 @@
-@import "tailwindcss";
-<script setup>
+<script lang="ts" setup>
 import TimerUi from "./component/TimerUi.vue";
-import toggleDark from "./component/toggleDark.vue";
+import Todo from "./component/todo.vue";
+import ToggleDark from "./component/toggleDark.vue";
 </script>
 
 <template>
-  <toggleDark />
-  <main>
-    <h1>Pomodoro Timer</h1>
-    <TimerUi />
-  </main>
+  <div class="animate-ping">
+  <div class="animate-spin">
+      <div class="animate-bounce">
+      <ToggleDark />
+      <main>
+        <h1>Pomodoro Timer</h1>
+        <TimerUi />
+        <Todo/>
+      </main>
+    </div>
+  </div>
+  </div>
 </template>
 
 <style>
--- pomodoro/src/component/todo.vue
+++ pomodoro/src/component/todo.vue
@@ -4,9 +4,11 @@
 </script>
 
 <template>
-  
+  <h1 class="text-xl">Hello testin</h1>
 </template>
 
 <style>
-
+/* h1{
+  font-size: 25px;
+} */
 </style>
